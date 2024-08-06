33736c1607ebd2c03a0b0a7adb89d47d96006716
Tumelo Mogono <tumelomogono@gmail.com>
2024-08-06 16:30:41 +0200

    created my sort which uses the store to apply sorting using the values from options.

---
+++ src/components/Sort.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import { ref } from 'vue';
+    import { useProductStore } from '../store/useProductStore';
+    
+    const store = useProductStore();
+    const selectedSort = ref(store.sorting);
+    
+    const applySort = () => {
+        store.setSorting(selectedSort.value);
+    };
+</script>
+
+<template>
+    <div>
+      <label for="sort">Sort by:</label>
+      <select id="sort" v-model="selectedSort" @change="applySort">
+        <option value="default">Default</option>
+        <option value="low">Price: Low to High</option>
+        <option value="high">Price: High to Low</option>
+      </select>
+    </div>
+</template>
+  
