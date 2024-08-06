4db4882a948788f2034cc8449a7b43491fd4db23
Tumelo Mogono <tumelomogono@gmail.com>
2024-08-06 16:34:22 +0200

    Created my Home page which uses the Filter, sort and productlist component, also using th fetchProducts on Mount to get the products.

---
+++ src/pages/Home.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import { onMounted } from 'vue';
+    import { useProductStore } from '../store/useProductStore';
+    import Filter from '../components/Filter.vue';
+    import Sort from '../components/Sort.vue';
+    import ProductList from '../components/ProductList.vue';
+
+    const store = useProductStore();
+
+    onMounted(() => {
+    store.fetchProducts();
+    });
+</script>
+
+<template>
+    <div>
+        <div class="grid lg:flex gap-y-4 gap-x-48 lg:items-start mt-3 mx-auto justify-center">
+            <Filter />
+            <Sort />
+        </div>
+        <ProductList />
+    </div>
+</template>
