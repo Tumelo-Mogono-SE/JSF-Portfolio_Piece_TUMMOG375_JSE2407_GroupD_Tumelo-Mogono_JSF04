dc78d5d9186d2b163c268df02ff1523054a2cd12
Tumelo Mogono <tumelomogono@gmail.com>
2024-08-06 16:52:05 +0200

    added create pinia and router to my main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
-import './tailwind.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import './tailwind.css';
+import App from './App.vue';
+import router from './router';
+import { createPinia } from 'pinia';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+const pinia = createPinia();
+app.use(pinia);
+app.use(router);
+
+app.mount('#app');
