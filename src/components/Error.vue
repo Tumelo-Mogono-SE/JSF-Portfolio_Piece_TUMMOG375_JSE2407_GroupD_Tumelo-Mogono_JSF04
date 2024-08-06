29e3f47bc83c1a0d5ada44176d31dcbcd200c43a
Tumelo Mogono <tumelomogono@gmail.com>
2024-08-06 16:04:22 +0200

    Added my error component to be used for errors.

---
+++ src/components/Error.vue
@@ -0,0 +1,9 @@
+<script>
+    let message = ref('Data fetching failed, please check your network connection')
+</script>
+
+<template>
+    <div class="mt-28 text-red-500 text-xl font-bold">
+        {{ message }}
+    </div>
+</template>
